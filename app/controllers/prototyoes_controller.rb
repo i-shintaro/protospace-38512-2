ed4f225039e525e8ee4ea6b1ef5eb1fb2f4092fa
i-shintaro <shintaroiwashita7857@gmail.com>
2024-08-25 17:07:48 +0900

    トップページ作成

---
+++ app/controllers/prototyoes_controller.rb
@@ -0,0 +1,5 @@
+class PrototyoesController < ApplicationController
+  def index
+    
+  end
+end
---
+++ test/controllers/prototyoes_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class PrototyoesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
