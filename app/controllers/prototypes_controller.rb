6d4150c9b292a6bb7c4f3cf4156a9eb78118d20f
i-shintaro <shintaroiwashita7857@gmail.com>
2024-09-07 18:45:38 +0900

    その他機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,8 @@
 class PrototypesController < ApplicationController
+
+  before_action :authenticate_user!, except:[:index, :edit, :destroy]
   before_action :set_prototype, only: [:show, :edit, :update]
+  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -39,12 +42,17 @@ class PrototypesController < ApplicationController
     end
   end
 
+  private
+
   def set_prototype
     @prototype = Prototype.find(params[:id])
   end
 
-  private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy ,:concept, :image).merge(user_id: current_user.id)
+  end
+
+  def contributor_confirmation
+    redirect_to root_path unless current_user == @prototype.user
   end
 end
